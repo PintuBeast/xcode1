8e90c54fb80082b5d9af0fb2b24a82550bd0e3b7
pintubeast <abhishek.jk1992@gmail.com>
2021-05-23 11:43:42 +0530

    Initial Commit

---
+++ trialViewControll/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  trialViewControll
+//
+//  Created by user194465 on 5/23/21.
+//  Copyright © 2021 ruggedgreen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
